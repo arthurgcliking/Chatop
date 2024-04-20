175c67f432b2fc086b2cdae148c60c8f532ec07e
arthurgcliking <arthur.gumery@cliking.co>
2024-04-20 20:02:27 +0200

    implemented JWT authentification

---
+++ src/main/java/com/chatop/Chatop/controllers/LoginController.java
@@ -0,0 +1,26 @@
+package com.chatop.Chatop.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.chatop.Chatop.services.JWTService;
+
+@RestController
+public class LoginController {
+
+
+	private JWTService jwtService;
+
+	public LoginController(JWTService jwtService) {
+		this.jwtService = jwtService;
+	}
+
+	@PostMapping("/login")
+	public String getToken(Authentication authentication) {
+        		String token = jwtService.generateToken(authentication);
+        		return token;
+	}
+
+}
